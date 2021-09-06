873528b966007dcd512fdc9bd928e7f3bd5d1f45
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-09-06 14:45:14 -0300

    build

--- src/validator.ts
+++ src/validator.ts
@@ -1,51 +1,57 @@
-import {Config, ConfigAuthAPI, ConfigAuthFTP } from 'types'
-import Logger from './logger'
+import { Config, ConfigAuthAPI, ConfigAuthFTP } from "sdz-agent-types";
+import Logger from "./logger";
 
-type Keys = Partial<ConfigAuthAPI | ConfigAuthFTP>
+type Keys = Partial<ConfigAuthAPI | ConfigAuthFTP>;
 
 export default class Validator {
-  private config: Config
+  private config: Config;
 
   constructor(config: Config) {
-    this.config = config
+    this.config = config;
   }
 
   auth() {
     try {
-      Logger.info(`VALIDANDO CONFIGURAÇÕES DE ${Logger.bold('AUTENTICAÇÃO DE API')}.`)
+      Logger.info(
+        `VALIDANDO CONFIGURAÇÕES DE ${Logger.bold("AUTENTICAÇÃO DE API")}.`
+      );
       if (!this.valid(this.config.auth.api)) {
-        throw new Error()
+        throw new Error();
       }
     } catch {
-      Logger.error('CONFIGURAÇÕES INVÁLIDAS.')
-      process.exit(1)
+      Logger.error("CONFIGURAÇÕES INVÁLIDAS.");
+      process.exit(1);
     }
     try {
-      Logger.info(`VALIDANDO CONFIGURAÇÕES DE ${Logger.bold('AUTENTICAÇÃO DE FTP')}.`)
+      Logger.info(
+        `VALIDANDO CONFIGURAÇÕES DE ${Logger.bold("AUTENTICAÇÃO DE FTP")}.`
+      );
       if (!this.valid(this.config.auth.ftp)) {
-        throw new Error()
+        throw new Error();
       }
     } catch {
-      Logger.warning('CONFIGURAÇÕES INVÁLIDAS.')
+      Logger.warning("CONFIGURAÇÕES INVÁLIDAS.");
     }
   }
 
   database() {
     try {
-      Logger.info(`VALIDANDO CONFIGURAÇÕES DE ${Logger.bold('BANCO DE DADOS')}.`)
+      Logger.info(
+        `VALIDANDO CONFIGURAÇÕES DE ${Logger.bold("BANCO DE DADOS")}.`
+      );
     } catch {
-      Logger.error('CONFIGURAÇÕES INVÁLIDAS.')
-      process.exit(1)
+      Logger.error("CONFIGURAÇÕES INVÁLIDAS.");
+      process.exit(1);
     }
   }
 
   valid(config: ConfigAuthAPI | ConfigAuthFTP): boolean {
     const valid = Object.keys(config).reduce((carrier, key) => {
       if (!config[key as keyof Keys]) {
-        return ++carrier
+        return ++carrier;
       }
-      return carrier
-    }, 0)
-    return !valid
+      return carrier;
+    }, 0);
+    return !valid;
   }
 }
