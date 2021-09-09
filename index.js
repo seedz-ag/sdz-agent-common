b7c0db1c0a6e9bc88c0703064f5fb3ed705782b0
Diogo <diogo.nunes>
2021-09-09 11:17:20 -0300

    Hydrator exportado

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Validator = exports.Logger = exports.datetime = void 0;
+
 const datetime_1 = __importDefault(require("./dist/datetime"));
 exports.datetime = datetime_1.default;
+const hydrator_1 = __importDefault(require("./dist/hydrator"));
+exports.Hydrator = hydrator_1.default;
 const logger_1 = __importDefault(require("./dist/logger"));
 exports.Logger = logger_1.default;
 const validator_1 = __importDefault(require("./dist/validator"));
