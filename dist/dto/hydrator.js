a09d7b523ed604f5184553aef49762120e0c72ac
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-09-09 10:54:11 -0300

    build

---
+++ dist/dto/hydrator.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Convert
+ *
+ * @param {object} mapping JSON DTO mapping
+ * @param {object} row     Database returned row
+ * @returns {object}       Mapped Entity
+ */
+const Hydrator = (mapping, row) => {
+    const hydrated = {};
+    Object.entries(mapping).map(([to, from]) => {
+        if (from) {
+            hydrated[to] = row[from];
+        }
+    });
+    return hydrated;
+};
+exports.default = Hydrator;
