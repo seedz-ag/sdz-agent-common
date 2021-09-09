38e6edaac134279767359cb97e4063ac0978c4c4
Diogo <diogo.nunes>
2021-09-09 10:27:10 -0300

    Hydrator é exportado

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import datetime from "./src/datetime";
+import Hydrator from "./src/dto/hydrator";
 import Logger from "./src/logger";
 import Validator from "./src/validator";
 
-export { datetime, Logger, Validator };
+export { datetime, Hydrator, Logger, Validator };
