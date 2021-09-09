21d988b0822f3d1fb0110855487ac3fd02bfeb75
Diogo <diogo.nunes>
2021-09-09 10:16:11 -0300

    Hydrator criado

---
+++ src/dto/hydrator.ts
@@ -0,0 +1,22 @@
+import { APIEntity, DatabaseRow, HydratorMapping } from "sdz-agent-types";
+
+/**
+ * Convert
+ *
+ * @param {object} mapping JSON DTO mapping
+ * @param {object} row     Database returned row
+ * @returns {object}       Mapped Entity
+ */
+const Hydrator = (mapping: HydratorMapping, row: DatabaseRow): APIEntity => {
+  const hydrated = {};
+
+  Object.entries(mapping).map(([to, from]) => {
+      if (from) {
+          hydrated[to] = row[from];
+      }
+  });
+
+  return hydrated;
+};
+
+export default Hydrator;
