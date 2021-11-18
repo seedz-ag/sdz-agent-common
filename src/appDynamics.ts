fd006b7dabeaa283b3c958733edb68fb968648ae
Diogo Nunes <diogo.nunes>
2021-11-18 13:10:11 -0300

    appDynamics refactored

--- src/appDynamics.ts
+++ src/appDynamics.ts
@@ -4,40 +4,31 @@ const env = (key: string): string | boolean | undefined => {
 
 export default (appd: any) => {
   const params = [
-    env("APPD_CONTROLLER_HOST_NAME"),
-    env("APPD_CONTROLLER_PORT"),
-    env("APPD_CONTROLLER_SSL_ENABLED"),
-    env("APPD_ACCOUNT_NAME"),
-    env("APPD_ACCOUNT_ACCESS_KEY"),
-    env("APPD_APPLICATION_NAME"),
-    env("APPD_TIER_NAME"),
-    env("APPD_NODE_NAME"),
+    "APPD_CONTROLLER_HOST_NAME",
+    "APPD_CONTROLLER_PORT",
+    "APPD_CONTROLLER_SSL_ENABLED",
+    "APPD_ACCOUNT_NAME",
+    "APPD_ACCOUNT_ACCESS_KEY",
+    "APPD_APPLICATION_NAME",
+    "APPD_TIER_NAME",
+    "APPD_NODE_NAME",
   ];
 
-  if (params.filter(e => !e).length) {
-    throw new Error('APPD Parameter missing.');
+  for (const param of params) {
+    if (!env(param)) {
+      throw new Error(`App Dynamics ${param} Parameter missing.`);
+    }
   }
 
-  const [
-    APPD_CONTROLLER_HOST_NAME,
-    APPD_CONTROLLER_PORT,
-    APPD_CONTROLLER_SSL_ENABLED,
-    APPD_ACCOUNT_NAME,
-    APPD_ACCOUNT_ACCESS_KEY,
-    APPD_APPLICATION_NAME,
-    APPD_TIER_NAME,
-    APPD_NODE_NAME,
-  ] = params;
-
   const config: any = {
-    controllerHostName: APPD_CONTROLLER_HOST_NAME,
-    controllerPort: APPD_CONTROLLER_PORT,
-    controllerSslEnabled: APPD_CONTROLLER_SSL_ENABLED,
-    accountName: APPD_ACCOUNT_NAME,
-    accountAccessKey: APPD_ACCOUNT_ACCESS_KEY,
-    applicationName: APPD_APPLICATION_NAME,
-    tierName: APPD_TIER_NAME,
-    nodeName: APPD_NODE_NAME,
+    controllerHostName: env('APPD_CONTROLLER_HOST_NAME'),
+    controllerPort: env('APPD_CONTROLLER_PORT'),
+    controllerSslEnabled: env('APPD_CONTROLLER_SSL_ENABLED'),
+    accountName: env('APPD_ACCOUNT_NAME'),
+    accountAccessKey: env('APPD_ACCOUNT_ACCESS_KEY'),
+    applicationName: env('APPD_APPLICATION_NAME'),
+    tierName: env('APPD_TIER_NAME'),
+    nodeName: env('APPD_NODE_NAME'),
     debug: true,
   };
 
