aac0099f44417b047194f69c512e64179b70731b
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-11-18 13:48:04 -0300

    build

---
+++ dist/appDynamics.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (appd: any) => any;
+export default _default;
---
+++ dist/appDynamics.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const env = (key) => {
+    return process.env[key];
+};
+exports.default = (appd) => {
+    const params = [
+        "APPD_CONTROLLER_HOST_NAME",
+        "APPD_CONTROLLER_PORT",
+        "APPD_CONTROLLER_SSL_ENABLED",
+        "APPD_ACCOUNT_NAME",
+        "APPD_ACCOUNT_ACCESS_KEY",
+        "APPD_APPLICATION_NAME",
+        "APPD_TIER_NAME",
+        "APPD_NODE_NAME",
+    ];
+    for (const param of params) {
+        if (!env(param)) {
+            throw new Error(`App Dynamics ${param} Parameter missing.`);
+        }
+    }
+    const config = {
+        controllerHostName: env("APPD_CONTROLLER_HOST_NAME"),
+        controllerPort: env("APPD_CONTROLLER_PORT"),
+        controllerSslEnabled: env("APPD_CONTROLLER_SSL_ENABLED"),
+        accountName: env("APPD_ACCOUNT_NAME"),
+        accountAccessKey: env("APPD_ACCOUNT_ACCESS_KEY"),
+        applicationName: env("APPD_APPLICATION_NAME"),
+        tierName: env("APPD_TIER_NAME"),
+        nodeName: env("APPD_NODE_NAME"),
+        debug: true,
+    };
+    appd.profile(config);
+    return appd;
+};
