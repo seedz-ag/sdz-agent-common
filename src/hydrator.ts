a39ff52fc5c689345d0f312dc354b9c47e991238
Wander Veloso <wander.veloso@seedz.ag>
2023-01-18 14:34:19 -0300

    feat: makes it possible to receive row values as objects in the hydrator

--- src/hydrator.ts
+++ src/hydrator.ts
@@ -6,11 +6,11 @@ const pipes = {
   Append: (row, value, append) => `${value || ``}${append}`,
   Capitalize: (row, value) => value && value.toUpperCase(),
   Concat: (row, value, field) => {
-    return value + (row[field] || '')
+    return value + (row[field] || "");
   },
   Prepend: (row, value, prepend) => `${prepend}${value || ``}`,
-  SetValue: (row, value, newValue) => newValue
-}
+  SetValue: (row, value, newValue) => newValue,
+};
 
 /**
  * Map column name to object correspondent attribute
@@ -22,49 +22,70 @@ const pipes = {
 const Hydrator = (mapping: HydratorMapping, row: DatabaseRow): APIEntity => {
   const hydrated = {};
   const rowKeys = {};
-  
-  const isValid = (input: any ):boolean => {
-    if(input === false || input === null || typeof input === 'undefined') {
-      return false
-    }
-    return true;
-  };
+  const rowNormalized = normalize(row);
 
-  Object.keys(row).map((key) => (rowKeys[`${key}`.toUpperCase()] = key));
+  Object.keys(rowNormalized).map((key) => {
+    if (typeof key === "object") {
+      return;
+    }
+
+    if (typeof rowNormalized[key] === "object") {
+      return Object.keys(rowNormalized[key]).map((subKey) => {
+        const newKey = `${key}-${subKey}`.toUpperCase();
+        rowKeys[newKey] = newKey;
+        rowNormalized[newKey] = rowNormalized[key][subKey];
+      });
+    }
+
+    return (rowKeys[`${key}`.toUpperCase()] = key);
+  });
 
   Object.entries(mapping).forEach(([to, from]) => {
-      let value = `${get(row, rowKeys[`${from}`.toUpperCase()], "")}`.trim();
-      if (to.match(/\|/)) {
-        const pipe = to.split(/\|/g)
-        to = pipe.shift();
-        pipe.forEach(pipe => {
-          const reg = new RegExp(/(.*?)\((.*?)\)/g);
-          const matches = reg.exec(pipe);
-          matches.shift();
-          const f = matches.shift();
-          const a = matches.shift().split(/,/g)
-          value = pipes[f](row, value, ...a)
-        })
-      }
-      hydrated[to] = value
-    });
+    let fromFormatted = from;
+
+    if (`${from}`.includes(".")) {
+      fromFormatted = `${from}`.replace(".", "-");
+    }
+
+    let value = `${get(
+      rowNormalized,
+      rowKeys[`${fromFormatted}`.toUpperCase()],
+      ""
+    )}`.trim();
+
+    if (to.match(/\|/)) {
+      const pipe = to.split(/\|/g);
+      to = pipe.shift();
+      pipe.forEach((pipe) => {
+        const reg = new RegExp(/(.*?)\((.*?)\)/g);
+        const matches = reg.exec(pipe);
+        matches.shift();
+        const f = matches.shift();
+        const a = matches.shift().split(/,/g);
+        value = pipes[f](row, value, ...a);
+      });
+    }
+    hydrated[to] = value;
+  });
+  console.log(hydrated);
+
   return hydrated;
 };
 
 const normalize = (obj: any): any => {
   return Object.keys(obj).reduce((carrier: any, to) => {
     switch (true) {
-      case (Array.isArray(obj[to])):
-        carrier[to] = obj[to].map(normalize)
+      case Array.isArray(obj[to]):
+        carrier[to] = obj[to].map(normalize);
         break;
-      case ('object' === typeof obj[to]):
-        carrier[to] = normalize(obj[to])
+      case "object" === typeof obj[to]:
+        carrier[to] = normalize(obj[to]);
         break;
-    default:
-      carrier[to.toUpperCase()] = obj[to];
+      default:
+        carrier[to.toUpperCase()] = obj[to];
     }
     return carrier;
   }, {});
-}
+};
 
 export default Hydrator;
