d1765df2766a2da0b189eab288217763fd8ca147
Wander Veloso <wander.veloso@seedz.ag>
2023-01-20 14:51:04 -0300

    chore: add build

--- dist/hydrator.js
+++ dist/hydrator.js
@@ -20,56 +20,35 @@ const pipes = {
 const Hydrator = (mapping, row) => {
     const hydrated = {};
     const rowKeys = {};
-    const rowNormalized = normalize(row);
-    Object.keys(rowNormalized).map((key) => {
-        if (typeof key === "object") {
-            return;
-        }
-        if (typeof rowNormalized[key] === "object") {
-            return Object.keys(rowNormalized[key]).map((subKey) => {
-                const newKey = `${key}-${subKey}`.toUpperCase();
-                rowKeys[newKey] = newKey;
-                rowNormalized[newKey] = rowNormalized[key][subKey];
-            });
-        }
-        return (rowKeys[`${key}`.toUpperCase()] = key);
+    const flattened = flatten(row);
+    let normalized = {};
+    Object.keys(flattened).map((key) => {
+        normalized = (0, dot_wild_1.set)(normalized, key, flattened[key]);
     });
     Object.entries(mapping).forEach(([to, from]) => {
-        let fromFormatted = from;
-        if (`${from}`.includes(".")) {
-            fromFormatted = `${from}`.replace(".", "-");
-        }
-        let value = `${(0, dot_wild_1.get)(rowNormalized, rowKeys[`${fromFormatted}`.toUpperCase()], "")}`.trim();
-        if (to.match(/\|/)) {
-            const pipe = to.split(/\|/g);
-            to = pipe.shift();
-            pipe.forEach((pipe) => {
-                const reg = new RegExp(/(.*?)\((.*?)\)/g);
-                const matches = reg.exec(pipe);
-                matches.shift();
-                const f = matches.shift();
-                const a = matches.shift().split(/,/g);
-                value = pipes[f](row, value, ...a);
-            });
-        }
-        hydrated[to] = value;
+        hydrated[to] = (0, dot_wild_1.get)(normalized, String(from).toUpperCase());
     });
-    console.log(hydrated);
     return hydrated;
 };
-const normalize = (obj) => {
-    return Object.keys(obj).reduce((carrier, to) => {
-        switch (true) {
-            case Array.isArray(obj[to]):
-                carrier[to] = obj[to].map(normalize);
-                break;
-            case "object" === typeof obj[to]:
-                carrier[to] = normalize(obj[to]);
-                break;
-            default:
-                carrier[to.toUpperCase()] = obj[to];
+const flatten = (obj) => {
+    let result = {};
+    for (const i in obj) {
+        if (Array.isArray(obj[i])) {
+            for (const j in obj[i]) {
+                const temp = flatten(obj[i][j]);
+                Object.keys(temp).forEach((key) => (result[`${j.toUpperCase()}.${key}`] = temp[key]));
+            }
         }
-        return carrier;
-    }, {});
+        else if (typeof obj[i] === "object") {
+            const temp = flatten(obj[i]);
+            for (const j in temp) {
+                result[i.toUpperCase() + "." + j.toUpperCase()] = temp[j];
+            }
+        }
+        else {
+            result[i.toUpperCase()] = obj[i];
+        }
+    }
+    return result;
 };
 exports.default = Hydrator;
